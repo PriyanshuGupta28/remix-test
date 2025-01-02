811e4dd381a4d44530749b6f2cd5b39a9dfa4815
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2025-01-02 18:22:54 +0530

    Update app.ts

--- my-react-router-app/server/app.ts
+++ my-react-router-app/server/app.ts
@@ -16,7 +16,7 @@ app.use(
     build: () => import("virtual:react-router/server-build"),
     getLoadContext() {
       return {
-        VALUE_FROM_VERCEL: "Hello from Vercel",
+        VALUE_FROM_VERCEL: "Hello from Priyanshu",
       };
     },
   })
